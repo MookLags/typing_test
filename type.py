b068588a754264906d40180b949764a9441c36b8
Mitchell <mookielagaras.gmail.com>
2025-10-01 12:12:36 -0400

    Move word generation logic into proper function

--- type.py
+++ type.py
@@ -44,21 +44,24 @@ def get_input() -> str:
 
   return key
 
-words = [fake.word() for _ in range(50)]
+def generate_words():
+  words = [fake.word() for _ in range(50)]
+  random_sentence = ' '.join([random.choice(words) for i in range(50)])
+  return random_sentence
 
-random_sentence = ' '.join([random.choice(words) for i in range(50)])
 sentence_length = len(random_sentence)
 key_strokes = 0
 print(random_sentence)
 while key_strokes != sentence_length:
   char_input = get_input()
-  if char_input == '\x7f':
-    print('\b \b', end='', flush=True)
+
+  if char_input == '\x7f': # Checks if key pressed is backspace
+    print('\b \b', end='', flush=True) # and handles accordingly
     key_strokes -= 1
-  else:
+  else: # Checking for any letter character
     if char_input == random_sentence[key_strokes]:
-      print(Back.GREEN + char_input, end='', flush=True)
+      print(Back.GREEN + char_input, end='', flush=True) # Green if correct
     else:
-      print(Back.RED + char_input, end='', flush=True)
+      print(Back.RED + char_input, end='', flush=True) # Red if incorrect
     key_strokes += 1
-print('\n\ngood job!')
+print('\n\ngood job!') # Placeholder until I get stats for typing test
